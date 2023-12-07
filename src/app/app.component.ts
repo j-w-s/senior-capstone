27bdddcab171570ee011dc2099934f2edd14f7c3
j-w-s <69388380+j-w-s@users.noreply.github.com>
2023-12-07 12:30:03 -0600

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent ],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, MatToolbarModule, MatButtonModule, MatIconModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'landing-page.component',
+  selector: 'landing-page',
   templateUrl: 'landing-page.component.html',
   styleUrls: ['landing-page.component.scss'],
   imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  standalone: true
 })
 export class LandingPageComponent { }
