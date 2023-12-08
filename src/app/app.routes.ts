6fdb2c3baf1f8f1f0607fe25b476c8af8ca1f784
j-w-s <69388380+j-w-s@users.noreply.github.com>
2023-12-08 16:09:27 -0600

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ExplorePageFrameComponent } from '../../src/app/components/explore-page/explore-page-frame';
+import { ExplorePageFrameComponent } from "../../src/app/components/explore-page/explore-page-frame/explore-page-frame.component";
 
 export const routes: Routes = [
   { path: 'explore', component: ExplorePageFrameComponent },
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -6,12 +6,13 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { Routes } from '@angular/router';
 
 @Component({
   selector: 'landing-page',
   templateUrl: 'landing-page.component.html',
   styleUrls: ['landing-page.component.scss'],
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatGridListModule, MatCardModule, FlexLayoutModule, MatExpansionModule,],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatGridListModule, MatCardModule, FlexLayoutModule, MatExpansionModule],
   standalone: true
 })
 export class LandingPageComponent {
