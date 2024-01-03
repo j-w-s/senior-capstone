bb1ffb4c466766a0e022667c538d12d6e8bbdc20
j-w-s <69388380+j-w-s@users.noreply.github.com>
2024-01-03 15:48:07 -0600

    login-register page

--- src/app/components/login-register/login-register.component.ts
+++ src/app/components/login-register/login-register.component.ts
@@ -1,73 +1,40 @@
-import { Component, OnInit } from '@angular/core'; import { trigger, state, style, animate, transition } from '@angular/animations';
-import { CommonModule } from '@angular/common';
-import { NgIf } from '@angular/common';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-register',
-  standalone: true,
-  imports: [NgIf, CommonModule, ReactiveFormsModule, MatInputModule,
-    MatButtonModule, MatFormFieldModule],
-  animations: [
-    trigger('slideInOut', [
-      state('in', style({
-        'max-height': '500px',
-        opacity: '1',
-        visibility: 'visible'
-      })),
-      state('out', style({
-        'max-height': '0px',
-        opacity: '0',
-        visibility: 'hidden'
-      })),
-      transition('in => out', [animate('400ms ease-in-out')]),
-      transition('out => in', [animate('400ms ease-in-out')])
-    ])
-  ],
+  imports: [ MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,     CommonModule,
+    ReactiveFormsModule],
+    standalone: true,
   templateUrl: './login-register.component.html',
-  styleUrl: './login-register.component.scss'
 })
-export class LoginRegisterComponent {
-  formType: string = 'login';
-  loginForm!: FormGroup;
-  registerForm!: FormGroup;
+export class LoginRegisterComponent implements OnInit {
+  formType = 'login';
 
-  constructor(private formBuilder: FormBuilder) { }
+  loginForm = this.fb.group({
+    usernameoremail: ['', Validators.required],
+    password: ['', Validators.required]
+  });
 
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      usernameOrEmail: ['', Validators.required],
-      password: ['', Validators.required]
-    });
+  registerForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    email: ['', Validators.required],
+    phonenumber: ['', Validators.required],
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+  });
 
-    this.registerForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      usernameOrEmail: ['', Validators.required],  
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
   }
 
-
-  onSubmit() {
-    if (this.formType === 'login') {
-      if (this.loginForm.invalid) {
-        return;
-      }
-
-    } else {
-      if (this.registerForm.invalid) {
-        return;
-      }
-
-    }
-  }
-
-  toggleForm() {
-    this.formType = this.formType === 'login' ? 'register' : 'login';
-  }
 }
