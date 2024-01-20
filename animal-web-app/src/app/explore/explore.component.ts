f3e07e63d2350c5c6698f294ebac64bba4618cae
j-w-s <69388380+j-w-s@users.noreply.github.com>
2024-01-19 18:09:58 -0600

    finished explore page template and added new widget for cordus's groups

--- animal-web-app/src/app/explore/explore.component.ts
+++ animal-web-app/src/app/explore/explore.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrl: './explore.component.scss'
 })
 export class ExploreComponent {
+  images = [
+    'https://www.randomlists.com/img/animals/octopus.webp',
+    'https://www.randomlists.com/img/animals/squirrel.webp',
+    'https://www.randomlists.com/img/animals/mouse.webp',
+    'https://www.randomlists.com/img/animals/tiger.webp',
+    'https://www.randomlists.com/img/animals/goat.webp',
+    'https://www.randomlists.com/img/animals/snowy_owl.webp',
+  ];
+
+  names = ['James', 'Robert', 'John', 'Michael', 'David', 'William', 'Richard', 'Joseph', 'Thomas', 'Christopher', 'Charles', 'Daniel', 'Matthew', 'Anthony', 'Mark', 'Donald', 'Steven', 'Andrew', 'Paul', 'Joshua', 'Kenneth', 'Kevin', 'Brian', 'George', 'Timothy', 'Ronald', 'Jason', 'Edward', 'Jeffrey', 'Ryan', 'Jacob', 'Gary', 'Nicholas', 'Eric', 'Jonathan', 'Stephen', 'Larry', 'Justin', 'Scott', 'Brandon', 'Benjamin', 'Samuel', 'Gregory', 'Alexander', 'Patrick', 'Frank', 'Raymond', 'Jack', 'Dennis', 'Jerry', 'Tyler', 'Aaron', 'Jose', 'Adam', 'Nathan', 'Henry', 'Zachary', 'Douglas'];
+
+  allCards = Array.from({ length: 79 }).map((_, i) => ({
+    id: i + 1,
+    title: `Title ${i + 1}`,
+    description: `Description for card ${i + 1}`,
+    animalName: this.names[Math.floor(Math.random() * this.names.length)],
+    image: this.images[Math.floor(Math.random() * this.images.length)],
+  }));
+
+
+  currentPage = 1;
+  cardsPerPage = 8;
+  rowsPerPage = 2;
+  cardsPerRow = 4;
+  totalPages = this.allCards.length / this.cardsPerPage;
+
+  gettotalPages(): number {
+    return Math.ceil(this.allCards.length / this.cardsPerPage);
+  }
+
+  getDisplayedCards(){
+    const start = (this.currentPage - 1) * this.cardsPerPage;
+    return this.allCards.slice(start, start + this.cardsPerPage);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  searchFunction(event: any) {
+    console.log(event.target.value);
+  }
 
 }
