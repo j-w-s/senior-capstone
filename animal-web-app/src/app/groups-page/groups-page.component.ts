c329ac44dcc5c0aa290b38d7f1b03a891f2ef5de
Cordus Bailey <cordusbailey02@gmail.com>
2024-01-25 02:23:20 -0600

    Added service and component functions for groups

--- animal-web-app/src/app/groups-page/groups-page.component.ts
+++ animal-web-app/src/app/groups-page/groups-page.component.ts
@@ -1,22 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DocumentReference } from '@angular/fire/compat/firestore';
+import { Subject, Subscription, takeUntil } from 'rxjs';
 import { GroupsService } from '../services/groups.service';
-import { LoginRegisterService } from '../services/login-register.service';
+
+// Group interface
+ export interface Group {
+  name: string;
+  description: string;
+  city: string;
+  state: string;
+  users: DocumentReference[];
+ }
 
 @Component({
   selector: 'app-groups-page',
   templateUrl: './groups-page.component.html',
   styleUrl: './groups-page.component.scss'
 })
-export class GroupsPageComponent {
+export class GroupsPageComponent implements OnInit {
+  // Stores all the groups retrieved from the database
+  groups: any[] = [];
+  // Stores the currently selected group from the carousel;
+  // used to display the group info at the bottom
+  selectedGroup: Group | null = null;
+  // Stores the resolved usernames from the group.users DocRef array
+  usernames: string[] | null = null;
+  private destroy$ = new Subject<void>();
+  private subscriptions: Array<Subscription> = [];
+ 
+  constructor(private groupService: GroupsService) { }
 
-  constructor(private groupsService: GroupsService, private loginRegService: LoginRegisterService) { }
-
-  ngOnInit() {
+  // Listener for document updates that the user is apart of.
+  // Needs a listener to update what groups th user
+  ngOnInit(): void {
+    // Subscribes the listeners to get the groups from the databse
+    // Returns the groups in the database whenever there is an update
+    this.groupService.getGroups().then((observable$) => {
+      observable$.pipe(takeUntil(this.destroy$)).subscribe(async (groups: any) => {
+         console.log('Received groups:');
+         groups.forEach(async (group: any) => {
+           console.log('Group ID:', group.useId);
+           //this.usernames = await this.groupService.resolveUsernames(group.users);
+         });
+         this.groups = groups;
+      }, error => {
+         console.error('Error getting groups:', error);
+      });
+     });
     
+    // Makes sure ngDestroy is called before reloaded a page
+    window.onbeforeunload = () => this.ngOnDestroy();
   }
 
-  isLoggedIn() {
-    return this.loginRegService.isLoggedIn
+  // Gets rid of the listeners when the page is destroyed (refreshed, unloaded, etc.)
+  ngOnDestroy() {
+    this.destroy$.next()
+    this.destroy$.complete()
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
+ 
 
+  // Sets the selected group from what was picked in the carousel
+  // Resolves the usernames to display
+  async selectGroup(group: Group) {
+    this.selectedGroup = group;
+    this.usernames = await this.groupService.resolveUsernames(group.users);
 }
+ 
+  // Creates a group using the input fields on the page
+  createGroup(name: string, description: string, city: string, state: string) {
+    // Creates a group to send and store in the database
+     const newGroup: Group = {
+       name: name,
+       description: description,
+       city: city,
+       state: state,
+       users: [],
+     };
+ 
+    // Calls the service function to create the group
+    // Adss the 'owner' of the group
+     this.groupService.createGroup(newGroup).then((groupRef) => {
+       this.addOwner(newGroup, groupRef.path)
+     });
+  }
+ 
+  // Adds a user to the group based on their username
+  addUser(group: any, username: string) {
+     this.groupService.addUserToGroup(group.useId, username);
+  }
+
+  // Adds the creator to the group using their userID
+  addOwner(group: Group, docRef: string) {
+    this.groupService.addOwner(docRef);
+  }
+ }
--- animal-web-app/src/app/services/groups.service.ts
+++ animal-web-app/src/app/services/groups.service.ts
@@ -1,14 +1,165 @@
 import { Injectable } from '@angular/core';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
-import { addDoc, collection, doc, getFirestore, runTransaction, setDoc } from '@angular/fire/firestore';
+import { getAuth } from "firebase/auth";
 import { LoginRegisterService } from './login-register.service';
-import { DocumentSnapshot, updateDoc } from 'firebase/firestore';
+import { AngularFirestore, DocumentReference } from '@angular/fire/compat/firestore';
+import { combineLatest, Observable } from 'rxjs';
+import { Group } from '../groups-page/groups-page.component';
+import { arrayUnion } from '@angular/fire/firestore';
+import { doc, DocumentData, getDoc, getFirestore, onSnapshot } from 'firebase/firestore';
 
 @Injectable({
-  providedIn: 'root'
+ providedIn: 'root'
 })
 export class GroupsService {
+  constructor(private db: AngularFirestore, private loginRegService: LoginRegisterService) { }
 
-  constructor() { }
+  // Sleep function to delay loading data until user is logged in
+  async sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+ 
+  
+  // Get groups function
+  // Returns an observable and array of groups to display groups
+  async getGroups(): Promise<Observable<unknown>> {
+    const auth = getAuth();
+    const user = auth.currentUser?.uid;
 
-}
+    await this.sleep(1000)
+   
+    if (!this.loginRegService.isLoggedIn) {
+       throw new Error('No user is currently signed in.');
+    }
+   
+    const userDocRef = doc(getFirestore(), 'User', this.loginRegService.currentUser);
+   
+    return new Observable(observer => {
+       const unsubscribe = onSnapshot(userDocRef, async (userDoc) => {
+         if (!userDoc.exists()) {
+           throw new Error('No such document!');
+         }
+   
+         const documentReferencesArray = userDoc.data()['groups'];
+         const observables = documentReferencesArray.map((docRef: any) => {
+           return new Observable(observer => {
+             const unsubscribe = onSnapshot(doc(getFirestore(), docRef.path), (doc) => observer.next({ useId: doc.id, ...doc.data() }));
+             return unsubscribe;
+           });
+         });
+   
+         combineLatest(observables).subscribe(groups => observer.next(groups));
+       });
+   
+       return unsubscribe;
+    });
+   }
+ 
+  // Create group and store in Groups collection. (Working)
+  createGroup(group: Group): Promise<DocumentReference<unknown>> {
+    // Takes in a group Object and adds it the the User collection
+    // Returns the document ID of the new document
+    return this.db.collection('Groups').add(group);
+  }
+ 
+  /*
+  // Update group
+  updateGroup(id: string, group: Partial<Group>): Promise<void> {
+     return this.db.collection('Groups').doc(id).update(group);
+  }
+ 
+  // Delete group
+  deleteGroup(id: string): Promise<void> {
+     return this.db.collection('Groups').doc(id).delete();
+  }
+  */
+
+  // Add user to group 
+  async addUserToGroup(groupRef: string, username: string)  {
+    // Placeholders for the user and path
+    let user: DocumentReference | null = null;
+    let uPath = ""
+
+    // Gets the user document ID and user ID
+    // sets them to the placeholders above
+    const userRef = this.db.collection('UsernameMapping').doc(username)
+    await userRef.get().toPromise().then(docData => {
+     if (docData && docData.exists) {
+        let data = docData.data() as {userID: DocumentReference};
+        user = data.userID;
+        uPath = user.path
+        console.log('Trying to add user: ' + user.path); 
+     } else {
+        console.log('No such document!');
+     }
+    });
+
+    // Adds the user to the Group document
+    const groupDocRef = this.db.collection('Groups').doc(groupRef);
+    groupDocRef.update({
+      users: arrayUnion(user)
+    }).then(() => {
+        console.log('Document successfully updated!');
+    }).catch((error) => {
+        console.error('Error updating document: ', error);
+    });
+
+    //Adds the group to the User document
+    const split = uPath.split("/");
+    const ref = this.db.collection(split[0]).doc(split[1]);
+    ref.update({
+      groups: arrayUnion(groupDocRef.ref)
+    }).then(() => {
+        console.log('Document successfully updated!');
+    }).catch((error) => {
+        console.error('Error updating document: ', error);
+    });
+
+
+  }
+
+  // Adds the owner to the group 
+  addOwner(groupRef: string) {
+    // Gets the userId of the current user
+    const auth = getAuth();
+    const user = auth.currentUser?.uid;
+    // Creates a document reference to the users collection
+    const userDocRef = this.db.doc('User/'+ user)
+    
+
+    // Split the groupRef at the "/" to make a document reference
+    const split = groupRef.split("/");
+    const groupDocRef = this.db.collection(split[0]).doc(split[1]);
+
+    // Updates the group collection to include the user
+    groupDocRef.update({
+      users: arrayUnion(userDocRef.ref)
+    }).then(() => {
+        console.log('Document successfully updated!');
+    }).catch((error) => {
+        console.error('Error updating document: ', error);
+    });
+
+    // Updates the user document by adding the new group document reference to the groups field
+    userDocRef.update({
+      groups: arrayUnion(groupDocRef.ref)
+    }).then(() => {
+        console.log('Document successfully updated!');
+    }).catch((error) => {
+        console.error('Error updating document: ', error);
+    });
+
+  }
+
+  // Resolves a user DocumentReference array into an array of usernames
+  async resolveUsernames(docRefs: DocumentReference<DocumentData>[]): Promise<string[]> {
+    const usernames: string[] = [];
+    for (let i = 0; i < docRefs.length; i++) {
+       const doc = await getDoc(docRefs[i]);
+       if (doc.exists()) {
+        // Gets the username stored in the document
+         usernames.push(doc.data()['username']); 
+       }
+    }
+    return usernames;
+   }
+ }
