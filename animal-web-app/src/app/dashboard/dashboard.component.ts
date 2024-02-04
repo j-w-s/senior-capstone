a37135d7661db51bd145ebd7176f4448bdf4ff21
j-w-s <69388380+j-w-s@users.noreply.github.com>
2024-02-04 00:05:26 -0600

    better dashboard, better marquee effect without the transitioning, etc (no longer custom but ready for production/easier to handle on client side)

--- animal-web-app/src/app/dashboard/dashboard.component.ts
+++ animal-web-app/src/app/dashboard/dashboard.component.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MessengerService } from '../services/messenger.service';
 import { getAuth } from 'firebase/auth';
 import User from '../../models/user';
 import { GroupsService } from '../services/groups.service';
+import Glide from '@glidejs/glide';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, AfterViewInit{
 
-  // Stores the primary User that is logged in
+  // stores the primary User that is logged in
   public primaryUser!: User;
 
   days = Array.from({ length: 5 }, (_, i) => Array.from({ length: 7 }, (__, j) => i * 7 + j + 1));
 
-  constructor(public messengerService: MessengerService, private groupsService: GroupsService) { }
+  constructor(public messengerService: MessengerService,
+    private groupsService: GroupsService){
 
-  // Gets the user logged in and resolves their userImage
+  }
+
+  // gets the user logged in and resolves their userImage
   async ngOnInit() {
     await this.groupsService.sleep(1000)
 
@@ -34,6 +38,42 @@ export class DashboardComponent {
       });
     }
 
+  }
+
+  // using glidejs to handle this now because my marquee version
+  // would sometimes flicker and required a lot of transformations
+  // on the front-end
+  ngAfterViewInit() {
+    var glide = new Glide('.glide', {
+      type: 'carousel',
+      autoplay: 1,
+      animationDuration: 4500,
+      animationTimingFunc: 'linear',
+      perView: 2.75,
+      breakpoints: {
+        1024: {
+          perView: 2
+        },
+        640: {
+          perView: 1,
+          gap: 36
+        }
+      },
+    });
+
+    glide.mount();
+
+    const slides = document.querySelectorAll('.glide__slide');
+
+    slides.forEach((slide) => {
+      slide.addEventListener('mouseover', () => {
+        glide.pause();
+      });
+
+      slide.addEventListener('mouseout', () => {
+        glide.play();
+      });
+    });
   }
 
 }
